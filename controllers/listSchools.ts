fd88af1d1edb7d55ab141eaa451cba55ae4325f1
Abhishek Kashyap <abhik5567@gmail.com>
2024-11-28 20:29:25 +0530

    feat: add save functionality for schools in /addSchool endpoint

--- controllers/listSchools.ts
+++ controllers/listSchools.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
-
+import { connection } from "../db";
 const listSchools = async (req: Request, res: Response) => {
     try {
-        return res.status(200).json({ success: true });
+        const [results, fields] = await (await connection).execute("SELECT * FROM school");
+        console.log(fields);
+        return res.status(200).json(results);
     } catch (error) {
         console.log(error);
     }
---
+++ types/school.d.ts
@@ -0,0 +1,6 @@
+export interface School {
+    name: string;
+    address: string;
+    latitude: Float32Array;
+    longitude: Float32Array;
+}
